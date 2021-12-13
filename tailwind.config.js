c0ca8dfb434f82fee7dd83ce8bbb63b91e8afda0
Pixel Papercraft <pixelpapercraft@gmail.com>
2021-12-13 16:24:56 +1100

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  purge: ["./src/**/*.bs.js"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.bs.js"],
 };
